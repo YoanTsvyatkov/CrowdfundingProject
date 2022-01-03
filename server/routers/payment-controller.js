2b1cb05064de977d20405d86534f4e7f6859121f
YoanTsvyatkov <YoanTsvyatkov@gmail.com>
2022-01-03 18:16:30 +0200

    minor changes

--- server/routers/payment-controller.js
+++ server/routers/payment-controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Stripe =require("stripe");
 
 const { Router } = express;
-const stripe = Stripe('pk_test_51KBLZ1He6HcPaami8OlgWuZD3dHu8kLCm3aqeZ6US9u7vYBi1RrO4D67tlqYey9f0TmQQy7UlBdy9UILBtdDiimt00RAngcDh3')
+const stripe = Stripe('sk_test_51KBLZ1He6HcPaamicMRz49bXwe8Rsz4he3z5ZR4mIEj8tC6ixXirdH7m4Rv5ROGWTNUj1isEyuA6PeFMglmrM2iA00HxZpMD4b')
 const paymentController = Router();
 
 paymentController.post("/checkout", async (req, res) => {
